7fb6a7fec0e57d0ebfe7c5d620e4ec37a3c0d2ac
TheUnixDaemon <theunixdaemon@gmail.com>
2025-04-07 14:45:49 +0200

    log created

--- App/Main.py
+++ App/Main.py
@@ -1,10 +1,16 @@
 from PathFile import PathFile
 from TransFile import TransFile
+from LogFile import LogFile
 from BatchTranslate import BatchTranslate
 
 class Main:
     def __init__(self):
+        self.__replaceDict: dict[str, str] = {
+            '"': r"\"",
+            "'": r"\'"
+        }
         self.__paths: list[str] = PathFile("filenames").read()
+        self.__logFile: LogFile = LogFile("logProgress.txt")
         self.__batchTranslate: BatchTranslate = BatchTranslate(r"[\u3040-\u30FF\u4E00-\u9FFF]+", r"(\{[^}]+\}|%[^%]+%)", ["PRINT"], ["{", "}"], 3000)
 
     # translate 'self.__paths' in lines 
